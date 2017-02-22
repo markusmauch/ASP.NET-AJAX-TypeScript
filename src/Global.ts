5b1be47fa05da0a2dd9014c254d60381d63d5153
unknown <mauchma@PC042980.corp.trumpf.com>
2017-02-22 13:08:19 +0100

    - Global added - Sys.UI.Bounds added

---
+++ src/Global.ts
@@ -0,0 +1,13 @@
+
+/**
+ * Provides a shortcut to the {@link getElementById} method of the {@link Sys.UI.DomElement} class.
+ * This member is static and can be invoked without creating an instance of the class.
+ * @param id
+ *      The ID of the DOM element to find.
+ * @param element
+ *      The parent element to search. The default is the document element.
+ */
+function $get( id: string, element: HTMLElement )
+{
+    return Sys.UI.DomElement.getElementById( id, element );
+}
