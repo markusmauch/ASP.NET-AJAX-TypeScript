f9ba635b0f67bea6c1b6a9bc1cfd9f9f12400e01
Markus Mauch <markus.mauch@gmail.com>
2017-02-22 20:18:57 +0100

    Update Type.ts

--- src/Type.ts
+++ src/Type.ts
@@ -3,8 +3,8 @@ interface Function
 {
     getName(): string;
     isInstanceOfType( instance: any ): boolean;
-    implementsInterface( interfaceType ): boolean;
-    inheritsFrom( parentType ): boolean;
+    implementsInterface( interfaceType: Function ): boolean;
+    inheritsFrom( parentType: Function ): boolean;
 }
 
 Function.prototype.getName = function()
@@ -15,6 +15,16 @@ Function.prototype.getName = function()
 Function.prototype.isInstanceOfType = function( instance: any )
 {
     return instance instanceof this;
+}
+
+Function.implementsInterface = function( interfaceType: Function )
+{
+	return this instanceof interfaceType;
+}
+
+Function.inheritsFrom = function( parentType: Function )
+{
+	return this instanceof parentType;
 }
 
 const Type = Function;
